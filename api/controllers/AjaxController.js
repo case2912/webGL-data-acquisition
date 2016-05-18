8b8bdfc953dbcd5364db220303e5213f396e765e
case2912 <shiroyama@maia.eonet.ne.jp>
2016-05-18 13:40:47 +0900

    modify ajax.js

--- api/controllers/AjaxController.js
+++ api/controllers/AjaxController.js
@@ -10,6 +10,5 @@ module.exports = {
     var params = req.allParams();
     res.send("This is from AjaxController!");
     console.log("Post request : record is done.");
-    ajax.create(ajax.define());
   }
 };
