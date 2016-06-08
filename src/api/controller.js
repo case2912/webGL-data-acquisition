96dbf67697a284e1d57a0d7cec5ceaa586427a8d
case2912 <shiroyama@maia.eonet.ne.jp>
2016-06-08 19:14:31 +0900

    modify controller

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,6 +1,25 @@
+import 'babel-polyfill';
 import koa from "koa";
+import RouterGenerator from "koa-router";
+const router = RouterGenerator();
 const app = new koa();
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+
+// async function checkWhatKoaDoing(ctx,next)
+// {
+//   console.log(ctx.req.originalUrl);
+//   if(ctx.req.originalUrl !== "/aaa"){
+//    ctx.body = "Rejected!!";
+//       return;
+//  }
+//    await next();
+//    console.log(ctx);
+// }
+
+router.get('/test', function(ctx, next) {
+    ctx.body = 'Hello World!';
 });
 app.listen(3000);
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+console.log('listening on port 3000');
